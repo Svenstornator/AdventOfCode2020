3bfac6e0ca89fde12ca6b4c4d89d1ab576d6a956
Stephen Watson <svenjwatson@gmail.com>
2020-12-15 12:45:19 +1030

    Day 8 completed

---
+++ Day8/Day8Task2.py
@@ -0,0 +1,56 @@
+inputFile = open("Day8Input", "r")
+inputData = inputFile.readlines()
+strippedInputData = [x[:-1] for x in inputData]
+inputData = strippedInputData
+
+command = []
+value = []
+endCondition = True
+
+for data in inputData:
+    data = data.split(" ")
+    command.append(data[0])
+    value.append(data[1])
+
+for individualCommand in range(len(command)):
+    # print("Iteration " + str(individualCommand))
+    if command[individualCommand] == 'jmp':
+        command[individualCommand] = 'nop'
+    elif command[individualCommand] == 'nop':
+        command[individualCommand] = 'jmp'
+
+    index = 0
+    executedIndices = []
+    count = 0
+
+    while(endCondition):
+        if index in executedIndices:
+            # print(count)
+            break
+        elif index >= len(command):
+            print("Successful Exit")
+            print(count)
+            break
+        else:
+            executedIndices.append(index)
+        if command[index] == 'acc':
+            if value[index][0] == '+':
+                count = count + int(value[index][1:])
+            elif value[index][0] == '-':
+                count = count - int(value[index][1:])
+            index = index + 1
+        elif command[index] == 'jmp':
+            if value[index][0] == '+':
+                index = index + int(value[index][1:])
+            elif value[index][0] == '-':
+                index = index - int(value[index][1:])
+        elif command[index] == 'nop':
+            executedIndices.append(index)
+            index = index + 1
+
+    if command[individualCommand] == 'jmp':
+        command[individualCommand] = 'nop'
+    elif command[individualCommand] == 'nop':
+        command[individualCommand] = 'jmp'
+
+
